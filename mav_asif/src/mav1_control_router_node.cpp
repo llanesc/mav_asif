903f41ad6676e1663f5c1f780c99fb0057693fe4
Christian Llanes <christian.llanes@gatech.edu>
2021-10-13 16:54:46 -0400

    asif calc is too high trying to reduce number of threads

--- mav_asif/src/mav1_control_router_node.cpp
+++ mav_asif/src/mav1_control_router_node.cpp
@@ -5,7 +5,7 @@ using namespace asif;
 int main(int argc, char *argv[]) {
     std::cout << "Starting MAV1 Control example..." << std::endl;
     Eigen::initParallel();
-    Eigen::setNbThreads(16);
+    Eigen::setNbThreads(2);
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MavControlRouter>(0));
